98418480d822abb63b3ffe3db59731618da09e3d
MichaelNicholas1 <mnicholas@albany.edu>
2020-12-02 15:26:09 -0500

    added composer/vendor files for heroku

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '9c88c21742b9e2352156032fa6caf05e87b5c7da',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9c88c21742b9e2352156032fa6caf05e87b5c7da',
+    ),
+  ),
+);
